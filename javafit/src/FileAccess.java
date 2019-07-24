b7e08b26db324328c836fad3364802d18b2e6e8a
Jorge Farinacci <43286515+farinacci1@users.noreply.github.com>
2019-07-23 23:35:42 -0500

    Add files via upload

--- javafit/src/FileAccess.java
+++ javafit/src/FileAccess.java
@@ -1,33 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 // Write code to load the workouts from the provided workouts.csv file. The function should return a Workouts object.
-import Config;
-import Workouts
+
 public class FileAccess {
   
   public static Workouts loadWorkouts() {
+	
     // What is a try/catch block and why do we need one?
     // What is an exception?
-		File file = new File(WORKOUTFILE);
-		Scanner scan = null;
-		
-			scan  = new Scanner(file);
-			while(scan.hasNext()) {
-				String line = scan.nextLine();
-				String[] tokens = line.split(",");
-				String name = tokens[0];
-				String equipment = tokens[1];
-				String primaryMuscle = tokens[2];
-				String secondaryMuscle = tokens[3];
-				String desc = tokens[4];
-				String reminders = tokens[5];
-				
-				muscle primary = muscle.valueof(primaryMuscle);
-				muscle secondary = muscle.valueof(secondaryMuscle);
-				Equipment equip = Equipment.valueof(equipment);
-				Workout workout =  new Workout(name,equip,primary,secondary,desc,remain);
-				Workouts.addWorkout(workout);
+	File file = new File(Config.WORKOUTFILE);
+	
+	Workouts workouts = new Workouts();
+	try 
+	{
+		Scanner scan = new Scanner(file);
+		String line = "";
+		while( scan.hasNextLine()) 
+		{
+			line = scan.nextLine();
+			String tokens[] = line.split(",");
+			String name = tokens[0];
+			Workouts.Equipment equipment = Workouts.Equipment.valueOf(tokens[1]);
+			Workouts.muscle primaryMuscle = Workouts.muscle.valueOf(tokens[2]);
+			Workouts.muscle secondaryMuscle = Workouts.muscle.valueOf(tokens[3]);
+			String desc = tokens[4];
+			String reminder = tokens[5];
 			
-			}
-			scan.close();
+			workouts.addWorkout(name, equipment,  primaryMuscle,  secondaryMuscle,  desc,  reminder);
+		}
+		scan.close();
+	} 
+	catch (FileNotFoundException e) 
+	{
+		e.printStackTrace();
+	}
+
+	
+	return workouts;
   }
 
 }
--- javafit/src/Workouts.java
+++ javafit/src/Workouts.java
@@ -13,7 +13,7 @@ public class Workouts {
   // Use the Refactor functionality in Eclipse to rename "muscle" to "Muscle" to match our naming convention.
   public enum muscle {ABS, BACK, BICEPS, CHEST, FOREARM, GLUTES, LOWERLEG, SHOULDER, TRICEPS, UPPERLEG, NONE} // Why didn't I have to declare this static?
   public enum Equipment {BARBELL, BODYWEIGHT, DUMBBELL, CABLE, HAMMERSTRENGTH}
-  private final ArrayList<Workout> workoutList = new ArrayList<Workout>();
+	private final ArrayList<Workout> workoutList = new ArrayList<Workout>();
 
 	// This is a nested class, also known as an inner class. Why do we use a nested class?
 	// You will need to create a number of methods for the inner class. You are not limited to 
@@ -26,24 +26,25 @@ public class Workouts {
 		private String desc;
 		private String reminders;
 	
-    Workout(String name, Equipment equipment, muscle primaryMuscle, muscle secondaryMuscle, String desc, String reminders) {
-    }
-		
-    // How do we get the name of an enumeration value?
+    Workout(String name, Equipment equipment, muscle primaryMuscle, muscle secondaryMuscle, String desc, String reminders) 
+    {
     	this.name = name;
     	this.equipment = equipment;
     	this.primaryMuscle = primaryMuscle;
     	this.secondaryMuscle = secondaryMuscle;
     	this.desc = desc;
     	this.reminders = reminders;
+    }
+		
+    // How do we get the name of an enumeration value?
 	}
 	
   // This function adds a new workout to the Workouts object.
   public final void addWorkout(String name, Equipment equipment, muscle primaryMuscle, muscle secondaryMuscle, String desc, String reminders)
   {
-		Workout workout =  new Workout(name,equipment,primaryMuscle,secondaryMuscle,desc,reminders);
-		addWorkout(workout);
-
+	  Workouts.Workout workout = new Workouts().new Workout(name,equipment,primaryMuscle,secondaryMuscle,desc,reminders);
+	  addWorkout(workout);
+	
   }
   
   // This function adds a workout to the Workouts object.
@@ -58,6 +59,21 @@ public class Workouts {
   // in the secondaryMuscle category.
 	public final Workouts getWorkoutsByMuscle(muscle m, boolean includeSecondary)
 	{
+		Workouts workouts = new Workouts();
+		for(Workouts.Workout work: this.workoutList) 
+		{
+			if(work.primaryMuscle.equals(m))
+			{
+				workouts.addWorkout(work);
+			}
+			else if(includeSecondary) 
+			{
+				if(work.secondaryMuscle.equals(m))
+					workouts.addWorkout(work);				
+			}
+			
+		}
+		return workouts;
 	  // What is short-circuit evaluation?
 	}
 	
@@ -65,23 +81,43 @@ public class Workouts {
 	// Equipment value that is provided as an argument.
   public final Workouts getWorkoutsByEquipment(Equipment e)
   {
+		Workouts workouts = new Workouts();
+		for(Workouts.Workout work: this.workoutList) 
+		{
+			if (work.equipment.equals(e)) 
+			{
+				workouts.addWorkout(work);
+			}
+		}
+	return workouts;
   }
 	
   // This returns a new Workouts object that contains only the workouts that contain an Equipment
   // value that is in the provided ArrayList of Equipment.
   public final Workouts getWorkoutsByEquipment(ArrayList<Equipment> e)
   {
+	  Workouts workouts = new Workouts();
+
+		  for(Equipment equip : e) 
+		  {
+			  
+			  workouts.workoutList.addAll(getWorkoutsByEquipment(equip).workoutList);
+			 	  
+		  }
+	  
+	  
+	return workouts;
   }
 	
   // This method returns an ArrayList of Strings. Each String is a name of a workout in our Workouts list.
   public final ArrayList<String> getNames()
   {
-	  ArrayList<String> workoutNames = new ArrayList<String>();
-	  
-	  for(Workout workout:workoutList) {
-		  workoutNames.add(workout->name);
+	  ArrayList<String> names = new ArrayList<String>();
+	  for(Workouts.Workout work: this.workoutList)  
+	  {
+		  names.add(work.name);
 	  }
-	  return workoutNames;
+	return names;
   }
  
   // This method returns all the information of the Workouts as an ArrayList of String arrays, 
@@ -89,5 +125,18 @@ public class Workouts {
   // Equipment, Primary and Secondary Muscles, Description, and Reminders. All of these should be strings.
   public final ArrayList<String[]> getFullInformation()
   {
+	  ArrayList<String[]> information = new ArrayList<String[]>();
+	  for(Workouts.Workout work: workoutList)  
+	  {
+		  String[] info =  new String[6];
+		  info[0]= work.name;
+		  info[1] = work.equipment.name();
+		  info[2] = work.primaryMuscle.name();
+		  info[3] = work.secondaryMuscle.name();
+		  info[4] = work.desc;
+		  info[5] = work.reminders;
+		  information.add(info);
+	}
+	return information;
   }
 }
